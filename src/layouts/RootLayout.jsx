604933688fa02bd65abad6813ff6f1b677d2d016
ahrorsoliev <akhrorteacher@gmail.com>
2024-01-06 18:40:41 -1200

    global container style added

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 function RootLayout() {
   return (
-    <div>
-      {/* NAVBAR */}
+    <div className="h-screen">
+      <Navbar />
       <Outlet />
-      {/* FOOTER */}
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <div>
+    <div className="max-container">
       <h1>Home Page</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere nam
